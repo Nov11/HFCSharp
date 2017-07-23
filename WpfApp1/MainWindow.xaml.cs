89abf22a4c8e9c1dea84ced2f188a535cb08234a
Nov11 <529079634@qq.com>
2017-07-24 04:24:21 +0800

    it's ok to bounce

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,8 +8,10 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace WpfApp1
 {
@@ -18,6 +20,7 @@ namespace WpfApp1
     /// </summary>
     public partial class Window1 : Window
     {
+        Random random = new Random();
         public Window1()
         {
             InitializeComponent();
@@ -25,7 +28,33 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            addEnemy();
+        }
 
+        private void addEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            object r = Resources["EnemyTemplate"];
+            enemy.Template = r as ControlTemplate;
+            animateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            animateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            playArea.Children.Add(enemy);
+        }
+
+        private void animateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard sb = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+
+            Storyboard.SetTarget(sb, enemy);
+            Storyboard.SetTargetProperty(sb, new PropertyPath(propertyToAnimate));
+            sb.Children.Add(animation);
+            sb.Begin();
         }
 
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
